7dfebbafc917f82125afecd072c946a74a49576b
dom-elves <dominic.elves@netmatters-scs.com>
2021-04-28 15:52:00 +0100

    function to get image added, basic layout mostly finished

--- Desktop/image-getter/app.js
+++ Desktop/image-getter/app.js
@@ -1,11 +1,18 @@
 function getimage(){
-  url = "https://unsplash.com/photos/random";
+  url = "https://dog.ceo/api/breeds/image/random";
 
   fetch(url)
     .then(function(response){
       return response.json();
     })
   .then(function(data){
-    console.log(data)
+    display_image(data.message);
   })
+  .catch(function(error){
+    console.log("error: " + error);
+  })
+}
+
+function display_image(image_url){
+  document.getElementById("image").src = image_url
 }
